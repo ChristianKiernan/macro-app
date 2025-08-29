4c3f296245213f2f7214215ad60822fd71f5b790
christiankiernan <clkiernan1@gmail.com>
2025-08-29 14:37:14 -0400

    fix: remove non-existent image field from auth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -62,7 +62,6 @@ export const authOptions: NextAuthOptions = {
           id: user.id,
           email: user.email,
           name: user.name,
-          image: user.image,
         };
       },
     }),
